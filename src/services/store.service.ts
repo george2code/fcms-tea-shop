687b5fc47fdadd211ffa634e86d08f94c817f2ba
george2code <george2code@gmail.com>
2026-08-20 10:46:56 +0300

    complete base structure

---
+++ src/services/store.service.ts
@@ -0,0 +1,46 @@
+import { axiosClassic, axiosWithAuth } from "@/api/api.interceptors";
+import { API_URL } from "@/config/api.config";
+import { IStore, IStoreCreate, IStoreUpdate } from "@/shared/types/store.interface";
+
+class StoreService {
+
+    async getById(id: string) {
+        const { data } = await axiosClassic<IStore>({
+            url: API_URL.stores(`/by-id/${id}`),
+            method: 'GET',
+        })
+
+        return data;
+    }
+
+    async create(data: IStoreCreate) {
+        const { data: createdStore } = await axiosWithAuth<IStore>({
+            url: API_URL.stores(''),
+            method: 'POST',
+            data,
+        })
+
+        return createdStore;
+    }
+
+    async update(id: string, data: IStoreUpdate) {
+        const { data: updatedStore } = await axiosWithAuth<IStore>({
+            url: API_URL.stores(`/${id}`),
+            method: 'PUT',
+            data,
+        })
+
+        return updatedStore;
+    }
+
+    async delete(id: string) {
+        const { data: deletedStore } = await axiosWithAuth<IStore>({
+            url: API_URL.stores(`/${id}`),
+            method: 'DELETE',
+        })
+
+        return deletedStore;
+    }
+}   
+
+export const storeService = new StoreService();
